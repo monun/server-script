3432454fda76c65fa061c47676440e879b192728
monun <monun1010@gmail.com>
2021-03-21 03:13:48 +0900

    :tada: 서버 스크립트 프로젝트 생성
    더 쉬운 서버 구성을 위해..

---
+++ paper/paper.sh
@@ -0,0 +1,123 @@
+#!/bin/sh
+
+download() {
+  download_result=$(wget -c --content-disposition -P "$2" -N "$1" 2>&1 | tail -2 | head -1)
+  echo "$download_result"
+}
+
+script=$(basename "$0")
+server="${script%.*}"
+config="./$server.conf"
+
+if [ ! -f "$config" ]
+then
+    cat << EOT > $config
+jar_url="https://papermc.io/api/v1/paper/1.16.5/latest/download"
+debug=false
+debug_port=5005
+backup=true
+restart=true
+memory=8
+plugins=(
+    'https://github.com/monun/kotlin-plugin/releases/latest/download/Kotlin-1.4.31.jar'
+    'https://github.com/monun/auto-update/releases/latest/download/AutoUpdate.jar'
+    'https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/target/ProtocolLib.jar'
+)
+EOT
+fi
+
+source "$config"
+
+# Print configurations
+echo "debug = $debug"
+echo "backup = $backup"
+echo "restart = $restart"
+echo "memory = ${memory}G"
+
+mkdir -p "./plugins"
+mkdir -p "$HOME/.minecraft/servers"
+
+# Download jar
+jar_result=$(download "$jar_url" "$HOME/.minecraft/servers")
+jar=$(grep -oG "‘.*’" <<< $jar_result)
+jar="${jar:1:-1}"
+echo "$jar_result"
+
+# Download plugins
+for i in "${plugins[@]}"
+do
+  download "$i" "./plugins"
+done
+
+jvm_arguments=(
+  "-Xmx${memory}G"
+  "-Xms${memory}G"
+  "-XX:+ParallelRefProcEnabled"
+  "-XX:MaxGCPauseMillis=200"
+  "-XX:+UnlockExperimentalVMOptions"
+  "-XX:+DisableExplicitGC"
+  "-XX:+AlwaysPreTouch"
+  "-XX:G1HeapWastePercent=5"
+  "-XX:G1MixedGCCountTarget=4"
+  "-XX:G1MixedGCLiveThresholdPercent=90"
+  "-XX:G1RSetUpdatingPauseTimePercent=5"
+  "-XX:SurvivorRatio=32"
+  "-XX:+PerfDisableSharedMem"
+  "-XX:MaxTenuringThreshold=1"
+  "-Dusing.aikars.flags=https://mcflags.emc.gs"
+  "-Daikars.new.flags=true"
+  "-Dcom.mojang.eula.agree=true"
+)
+
+if (($memory < 12))
+then
+  jvm_arguments+=(
+    "-XX:G1NewSizePercent=30"
+    "-XX:G1MaxNewSizePercent=40"
+    "-XX:G1HeapRegionSize=8M"
+    "-XX:G1ReservePercent=20"
+    "-XX:InitiatingHeapOccupancyPercent=15"
+ )
+else
+  echo "Enable high-capacity memory jvm options"
+  jvm_arguments+=(
+    "-XX:G1NewSizePercent=40"
+    "-XX:G1MaxNewSizePercent=50"
+    "-XX:G1HeapRegionSize=16M"
+    "-XX:G1ReservePercent=15"
+    "-XX:InitiatingHeapOccupancyPercent=20"
+  )
+fi
+
+if ($debug)
+then
+  jvm_arguments+=("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:$debug_port")
+fi
+
+jvm_arguments+=(
+  "-jar"
+  "$jar"
+  "--nogui"
+)
+
+while :
+do
+  java "${jvm_arguments[@]}"
+
+  if ($backup)
+  then
+    read -r -t 5 -p "Press Enter to start the backup immediately `echo $'\n> '`"
+    echo 'Start the backup.'
+    backup_file_name=$(date +"%y%m%d-%H%M%S")
+    mkdir -p '.backup'
+    tar --exclude='./.backup' --exclude='*.gz' --exclude='./cache' -zcf ".backup/$backup_file_name.tar.gz" .
+    echo 'The backup is complete.'
+  fi
+
+  if (! ($restart))
+  then
+    break
+  fi
+
+  read -r -t 5 -p "The server restarts. Press Enter to start immediately or Ctrl+C to cancel `echo $'\n> '`"
+done
