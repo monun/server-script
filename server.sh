44723a59e411eacaca878b5ef0f322fba85a6ce4
monun <monun1010@gmail.com>
2021-06-10 09:12:41 +0900

    ProtocolLib 다운로드 제거 (민폐) + 메모리 16기가

--- server.sh
+++ server.sh
@@ -3,9 +3,7 @@
 server=paper
 version=1.16.5
 plugins=(
-    'https://github.com/monun/kotlin-plugin/releases/latest/download/Kotlin-1.4.32.jar'
-    'https://github.com/monun/auto-update/releases/latest/download/AutoUpdate.jar'
-    'https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/target/ProtocolLib.jar'
+    'https://github.com/monun/kotlin-plugin/releases/latest/download/Kotlin-1.5.10.jar'
 )
 
 script=$(basename "$0")
@@ -26,7 +24,7 @@ debug=false
 debug_port=5005
 backup=true
 restart=true
-memory=8
+memory=16
 plugins=(
 EOF
     for plugin in "${plugins[@]}"
