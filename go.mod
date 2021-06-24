e43fbbdc1b54cc1b8effe8cb972442f63d8ea5e7
aroxu <30624945+aroxu@users.noreply.github.com>
2021-06-24 17:56:33 +0900

    윈도우 콘솔 출력 문제 수정

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/cavaliercoder/grab v2.0.0+incompatible
 	github.com/cheggaaa/pb v1.0.29
+	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 )
--- utils/logger/panic.go
+++ utils/logger/panic.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/mattn/go-colorable"
 	customLog "github.com/sirupsen/logrus"
 )
 
@@ -12,7 +13,9 @@ func Panic(content string) {
 	content = fmt.Sprintf("%v ", time.Now().Format("15:04:05")) + content
 
 	customLog.SetFormatter(&customLog.TextFormatter{
+		ForceColors:      true,
 		DisableTimestamp: true,
 	})
+	customLog.SetOutput(colorable.NewColorableStdout())
 	customLog.Panicf(content)
 }
