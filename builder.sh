a379a16a6acb2cb64154290a03d4b9fae9bfd31a
aroxu <30624945+aroxu@users.noreply.github.com>
2021-06-24 13:07:54 +0900

    빌드 구조 변경

--- builder.sh
+++ builder.sh
@@ -1,10 +1,6 @@
-mkdir -p build/server_darwin_x64
-mkdir -p build/server_linux_x64
-mkdir -p build/server_linux_x86
-mkdir -p build/server_win_x64
-mkdir -p build/server_win_x86
-GOOS=darwin GOARCH=amd64 go build -o build/server_darwin_x64/server
-GOOS=linux GOARCH=amd64 go build -o build/server_linux_x64/server
-GOOS=linux GOARCH=386 go build -o build/server_linux_x86/server
-GOOS=windows GOARCH=amd64 go build -o build/server_win_x64/server.exe
-GOOS=windows GOARCH=386 go build -o build/server_win_x86/server.exe
+mkdir -p build
+GOOS=darwin GOARCH=amd64 go build -o build/server_darwin_x64
+GOOS=linux GOARCH=amd64 go build -o build/server_linux_x64
+GOOS=linux GOARCH=386 go build -o build/server_linux_x86
+GOOS=windows GOARCH=amd64 go build -o build/server_win_x64.exe
+GOOS=windows GOARCH=386 go build -o build/server_win_x86.exe
