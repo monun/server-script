42bdf0f450bccd8db4c2a90c0c73d5133843dc75
dolphin2410 <80996692+dolphin2410@users.noreply.github.com>
2021-07-14 22:06:02 +0900

    url regex 업데이트
    url이 갖을 수 없는 문자를 허용하는 regex 교체. 프로토콜 없이도 작동하도록 수정.

--- .server/server.sh
+++ .server/server.sh
@@ -50,7 +50,7 @@ if [ "$server" = "." ]; then
 elif [ -f "$server" ]; then
   jar=$server
 else
-  url_regex='(https?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+  url_regex='((https?):\/\/)?([a-zA-Z])+(\.([a-zA-Z]+))+(\/.*)*?$'
   if [[ $server =~ $url_regex ]]; then
     jar_folder="$HOME/.minecraft/server/"
     mkdir -p "$jar_folder"
